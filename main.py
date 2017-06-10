e412a2438d26e5faac8fee9dd50b5b3e4bd12b56
Sandeep Datta <datta.sandeep@gmail.com>
2017-06-10 11:37:00 +0530

    First mindmap.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 import sys
+from typing import List
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, \
     QHBoxLayout, QVBoxLayout, QSizePolicy, QScrollArea
-from PyQt5.QtGui import QIcon, QPaintEvent, QPainter, QPen
+from PyQt5.QtGui import QIcon, QPaintEvent, QPainter, QPen, QPainterPath
 from PyQt5.QtCore import Qt, QPoint
 
 root_tree = ["Root node",
@@ -16,6 +18,9 @@ class NodeWidget(QWidget):
         super(NodeWidget, self).__init__()
         self.lblRoot = QLabel(node_tree[0])
         self.vboxChildren = QVBoxLayout()
+        self.children: List[NodeWidget] = []
+        self.blackPen = QPen(Qt.black, 2)
+        self.redPen = QPen(Qt.red, 4)
         self.initUi(node_tree)
 
     def initUi(self, node_tree):
@@ -38,6 +43,7 @@ class NodeWidget(QWidget):
             for child_tree in children:
                 child = NodeWidget(child_tree)
                 self.vboxChildren.addWidget(child)
+                self.children.append(child)
             hbox.addLayout(self.vboxChildren)
         except IndexError:
             pass
@@ -45,7 +51,7 @@ class NodeWidget(QWidget):
     def paintEvent(self, event: QPaintEvent):
         with QPainter(self) as painter:
             painter.setRenderHint(QPainter.Antialiasing, True)
-            painter.setPen(QPen(Qt.black, 2))
+            painter.setPen(self.blackPen)
 
             rect = self.lblRoot.geometry().translated(0, 2)
 
@@ -56,6 +62,25 @@ class NodeWidget(QWidget):
             p += QPoint(width, 0)
 
             painter.drawLine(rect.bottomLeft(), p)
+
+            for child in self.children:
+                childBottomLeft : QPoint = child.mapToParent(child.lblRoot.geometry().bottomLeft())
+                childBottomLeft += QPoint(0, 2)
+
+                cpX = rect.right() + int((childBottomLeft.x() - rect.right()) / 2)
+
+                start = QPoint(rect.bottomRight())
+
+                cp1 = QPoint(cpX, rect.bottom())
+                cp2 = QPoint(cpX, childBottomLeft.y())
+                end = QPoint(childBottomLeft)
+
+                path = QPainterPath()
+                path.moveTo(start)
+                path.cubicTo(cp1, cp2, end)
+                painter.drawPath(path)
+
+
 
 
 class MainWindow(QMainWindow):
