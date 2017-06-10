4d4d208f9ceeda2c4780957e592c9c79f163e049
Sandeep Datta <datta.sandeep@gmail.com>
2017-06-10 11:09:52 +0530

    Now drawing underlines beneath the labels.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, \
     QHBoxLayout, QVBoxLayout, QSizePolicy, QScrollArea
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QPaintEvent, QPainter, QPen
+from PyQt5.QtCore import Qt, QPoint
 
 root_tree = ["Root node",
                 [["Child Node1"],
@@ -13,28 +14,48 @@ root_tree = ["Root node",
 class NodeWidget(QWidget):
     def __init__(self, node_tree):
         super(NodeWidget, self).__init__()
+        self.lblRoot = QLabel(node_tree[0])
+        self.vboxChildren = QVBoxLayout()
         self.initUi(node_tree)
 
     def initUi(self, node_tree):
-        lblRoot = QLabel(node_tree[0])
-        lblRoot.setStyleSheet("border: 1px solid green")
         self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        #self.lblRoot.setStyleSheet("border: 1px solid green")
+
+        vbox1 = QVBoxLayout()
+        vbox1.addStretch()
+        vbox1.addWidget(self.lblRoot)
+        vbox1.addStretch()
 
         hbox = QHBoxLayout()
-        hbox.addWidget(lblRoot)
+        hbox.addLayout(vbox1)
 
         self.setLayout(hbox)
-        self.setStyleSheet("border: 1px solid red")
+        #self.setStyleSheet("border: 1px solid red")
 
         try:
             children = node_tree[1]
-            vbox = QVBoxLayout()
             for child_tree in children:
                 child = NodeWidget(child_tree)
-                vbox.addWidget(child)
-            hbox.addLayout(vbox)
+                self.vboxChildren.addWidget(child)
+            hbox.addLayout(self.vboxChildren)
         except IndexError:
             pass
+
+    def paintEvent(self, event: QPaintEvent):
+        with QPainter(self) as painter:
+            painter.setRenderHint(QPainter.Antialiasing, True)
+            painter.setPen(QPen(Qt.black, 2))
+
+            rect = self.lblRoot.geometry().translated(0, 2)
+
+            # Draw the underline
+            width = self.lblRoot.fontMetrics().boundingRect(self.lblRoot.text()).width()
+
+            p = rect.bottomLeft()
+            p += QPoint(width, 0)
+
+            painter.drawLine(rect.bottomLeft(), p)
 
 
 class MainWindow(QMainWindow):
@@ -47,7 +68,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('Mindforge')
         self.setWindowIcon(QIcon('icons/engineering.svg'))
-        self.statusBar().showMessage('Ready')
+        self.statusBar().showMessage('New mindmap')
         sa = QScrollArea()
         w = NodeWidget(root_tree)
         sa.setWidget(w)
